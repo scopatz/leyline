0a7e4588a5f441a9b1da974dfded02d88fd76d7e
Anthony Scopatz <scopatz@gmail.com>
2018-01-30 17:57:08 -0500

    more pyghooks

--- leyline/pyghooks.py
+++ leyline/pyghooks.py
@@ -2,9 +2,11 @@
 import re
 
 from pygments.lexers import get_lexer_by_name
-from pygments.lexer import RegexLexer, bygroups, using
+from pygments.lexer import RegexLexer, bygroups, using, this
 from pygments.lexers.agile import PythonLexer
-from pygments.token import Punctuation, Keyword, Comment, Text, Operator, String
+from pygments.lexers.markup import TexLexer
+from pygments.token import (Punctuation, Keyword, Comment, Text, Operator,
+    String, Escape, Generic, Literal, Name)
 from pygments.util import ClassNotFound
 
 
@@ -46,9 +48,59 @@ class LeylineLexer(RegexLexer):
 
     tokens = {
         'root': [
+            # multiline comments
             (r'[#][#][#][^#\\]*(?:(?:\\.|[#](?![#][#][#]))[^[#]\\]*)*[#][#][#]',
              Comment.Multiline),
+            # code blocks
             (r"```([^`\\]*(?:(?:\\.|`(?!``))[^`\\]*)*)```", codeblock_callback),
-            (r'[^\n]+', Text),
+            # mutliline math
+            (r"(\$\$\$)([^\$\\]*(?:(?:\\.|\$(?!\$\$))[^\$\\]*)*)(\$\$\$)",
+             bygroups(String, using(TexLexer), String)),
+            # singleline comment
+            (r'[#][^\r\n]*', Comment),
+            # inline code
+            (r"(`)([^\n`\\]*(?:\\.[^\n`\\]*)*)(`)",
+             bygroups(String, using(PythonLexer), String)),
+            # inline math
+            (r"(\$)([^\n\$\\]*(?:\\.[^\n\$\\]*)*)(\$)",
+             bygroups(String, using(TexLexer), String)),
+            # escape chars
+            (r'{%}|{%|%}|[{][{]|[}][}]', String.Escape),
+            # strikethrough
+            (r'--.*?--', Generic.Deleted),
+            # bold
+            (r'\*\*.*?\*\*', Operator.Word),
+            # italics
+            (r'~~.*?~~', Name.Function),
+            # underscore
+            (r'__.*?__', Name.Class),
+            # subscript
+            (r'({_)(.*?)(_})', bygroups(Name.Class, Text, Name.Class)),
+            # superscript
+            (r'({\^)(.*?)(\^})', bygroups(Name.Class, Text, Name.Class)),
+            # rend
+            (r'(rend)((?: +[A-Za-z_][A-Za-z0-9_]*)+)(::)',
+             bygroups(Keyword.Reserved, Name.Namespace, Operator)),
+            # with
+            ('(with)(| +[A-Za-z_][A-Za-z0-9_]*)(::)',
+             bygroups(Keyword.Reserved, Name.Namespace, Operator), 'withblock'),
+            # table
+            (r'(table)(::)', bygroups(Keyword.Reserved, Operator), 'tableblock'),
+            # figure
+            (r'(figure)(::)', bygroups(Keyword.Reserved, Operator)),
+            # plain text
+            (r'[^-\n#`${}%*~_^:]+', Text),
             ],
-    }
+        # go into python mode for with block
+        'withblock': [
+            (r'[^\n]+', using(PythonLexer)),
+            (r'\n[ \t]+', Text),
+            (r'\n[^ \t]', using(this), '#pop')
+            ],
+        # go into python mode for first part of table block
+        'tableblock': [
+            (r'[^\n]+', using(PythonLexer)),
+            (r'\n[ \t]+[^*-]', using(PythonLexer)),
+            (r'\n[ \t]+[*-] ', using(this), '#pop')
+            ],
+        }
